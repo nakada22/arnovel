9ef208b9ef9a3a5dd0027a9d32102a599c231fda
nakada <nakada22@gmail.com>
2013-07-22 00:41:50 +0900

    新規作成

---
+++ src/com/tetuo41/arnovel/TopActivity.java
@@ -0,0 +1,38 @@
+package com.tetuo41.arnovel;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+ 
+public class TopActivity extends Activity implements View.OnClickListener{
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        //Log.e(this.getClass().getName(),"onCreate");
+    	super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.top);
+		
+    }
+    
+    /*
+     * クリック時の処理
+     * 
+     * */
+    
+    
+    public void onClick(View v) {
+		switch (v.getId()) {
+//		case R.id.start:
+//			// STARTボタンんクリック時
+//			break;
+//		case R.id.record:
+//			// RECORDボタンクリック時
+//			break;
+//		default:
+//			break;
+		}
+	}
+}
